724016d1d680afbc7c5460907f324c0be85c9848
HARIHARAN M V <harikvg031207@gmail.com>
2026-04-27 09:48:49 +0530

    vvv2

--- service-worker.js
+++ service-worker.js
@@ -2,14 +2,29 @@ const CACHE_NAME = "calculator-v2";
 
 const urlsToCache = [
   "./",
-  "./index.html"
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting(); // activate immediately
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache); // delete old cache
+          }
+        })
+      )
+    )
   );
 });
 
